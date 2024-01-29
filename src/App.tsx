a86a4e298412e212fa3d857d124cdece6ac7fa5d
unknown <gojh1013@gmail.com>
2024-01-29 17:39:41 +0900

    feat: msw 목업완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [data, setData] = useState('');
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/resource', {
+                    method: 'GET',
+                });
+                if (response.ok) {
+                    const result = await response.text();
+                    console.log('Result: ', result);
+                    setData(result);
+                }
+            } catch (error) {
+                console.error('Error', error);
+            }
+        };
+
+        fetchData();
+    }, []);
     return (
-        <>
+        <div>
+            <div>MSW로 받아온 데이터</div>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <pre>{data}</pre>
             </div>
-            <h1>깃 연동 확인</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        </div>
     );
 }
 
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,22 +1,7 @@
-import { http } from 'msw';
-
-const todos = ['먹기', '자기', '놀기'];
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-    // 테스트 용
-    http.get('/ping', (req, res, ctx) => {
-        console.log('Request:', req);
-        return res(ctx.json('pong'));
-    }),
-
-    // 할일 목록
-    http.get('/todos', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json(todos));
-    }),
-
-    // 할일 추가
-    http.post('/todos', (req, res, ctx) => {
-        todos.push(req.body);
-        return res(ctx.status(201));
+    http.get('/resource', () => {
+        return HttpResponse.text('Hello world!');
     }),
 ];
