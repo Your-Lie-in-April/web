bdb586e188f966a353d54e20113fcdd4a5d777c9
unknown <gojh1013@gmail.com>
2024-02-02 15:45:39 +0900

    test: Nickname 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,119 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, ChangeEventHandler } from 'react';
 
 function App() {
-    useEffect(() => {
-        const Oauth = async () => {
-            const response = await fetch('/api/v1/oauth2/status', {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-                },
-            });
-            if (response.ok) {
-                const data = await response.json();
-                console.log('Oauth status', data);
-            }
-        };
-        Oauth();
+    const [memberId, setMemberId] = useState<string>();
+    const [status, setStatus] = useState<string>();
+    const [nickname, setNickname] = useState<string>();
 
-        const login = async () => {
-            const body = {
-                email: 'user@example.com',
-                password: 'password123',
-            };
-            const response = await fetch('/api/v1/oauth2/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body),
-                credentials: 'include',
+    useEffect(() => {
+        //회원 전체 조회
+        const Members = async () => {
+            const response = await fetch('/v1/members/all', {
+                method: 'GET',
             });
             if (response.ok) {
                 const data = await response.json();
-                console.log('login', data);
-            } else {
-                console.error('에러');
+                console.log('회원목록', data);
             }
         };
-        login();
+        Members();
+        //스케쥴 전체 조회
+        const Schedules = async () => {
+            const response = await fetch('/v1/schedules/all', {
+                method: 'GET',
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('스케쥴목록', data);
+            }
+        };
+        Schedules();
     }, []);
+
+    //회원 정보 조회
+    const MemberId = async () => {
+        const response = await fetch(`/v1/members/${memberId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log('회원정보', data);
+        } else {
+            console.error('에러');
+        }
+    };
+    const handleMemeberId: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setMemberId(e.target.value);
+    };
+    const Seemember = () => {
+        MemberId();
+    };
+
+    //상태메시지 설정
+    const Status = async () => {
+        const response = await fetch(`/v1/members/${status}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+            },
+            body: JSON.stringify({ status: status }),
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log('상태메시지 설정', data);
+        } else {
+            console.error('에러');
+        }
+    };
+    const handleStatus: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setStatus(e.target.value);
+    };
+    const SeeStatus = () => {
+        Status();
+    };
+
+    //닉네임 재설정
+    const Nickname = async () => {
+        const response = await fetch('/v1/projects/members/nickname', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+            },
+            body: JSON.stringify({ projectId: 1, nickname: nickname }),
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log('닉네임 재설정', data);
+        } else {
+            console.error('에러');
+        }
+    };
+
+    const handleNickname: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setNickname(e.target.value);
+    };
+    const SeeNickname = () => {
+        Nickname();
+    };
+
     return (
         <div>
             <div>MSW로 받아온 데이터</div>
+            <br />
+            <input value={memberId} onChange={handleMemeberId}></input>
+            <button onClick={Seemember}>회원정보 확인</button>
+            <br />
+            <input value={status} onChange={handleStatus}></input>
+            <button onClick={SeeStatus}>상태메시지 변경</button>
+            <br />
+            <input value={nickname} onChange={handleNickname}></input>
+            <button onClick={SeeNickname}>닉네임 변경</button>
         </div>
     );
 }
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,25 +2,127 @@ import { http, HttpResponse } from 'msw';
 
 const token =
     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpbnUxMjM0Iiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImNsaWVudE51bSI6MSwiaWF0IjoxNzA1OTQ1OTQxLCJleHAiOjE3MDU5NDk1NDF9.Cs95IQ12uo2WGIrv21k-7UF27Rj6g5uKj36_mVrVcXI';
+const List = [
+    {
+        memberId: 1,
+        email: 'example@example.com',
+        nickname: 'namu',
+        state: '완벽한 하루',
+        profileImageURL: 'https://xxx.kakao.co.kr/.../aaa.jpg',
+    },
+    {
+        memberId: 2,
+        email: 'example2@example.com',
+        nickname: 'gojaeng',
+        state: '화이팅',
+        profileImageURL: 'https://xxx.kakao.co.kr/.../aaa.jpg',
+    },
+    {
+        memberId: 3,
+        email: 'example3@example.com',
+        nickname: 'milk',
+        state: '우유',
+        profileImageURL: 'https://xxx.kakao.co.kr/.../aaa.jpg',
+    },
+];
+
+const ScheduleList = [
+    {
+        nickname: 'namu',
+        schedule: [
+            {
+                dayOfWeek: 'mon',
+                schedule: [
+                    { startAt: '2024-01-31T17:00:00+09:00', endAt: '2024-01-31T20:30:00+09:00' },
+                    { startAt: '2024-01-31T21:30:00+09:00', endAt: '2024-01-31T22:30:00+09:00' },
+                ],
+            },
+            {
+                dayOfWeek: 'tue',
+                schedule: [{ startAt: '2024-02-4T10:30:00+09:00', endAt: '2024-02-4T11:30:00+09:00' }],
+            },
+            {
+                dayOfWeek: 'wed',
+                schedule: [{ startAt: '2024-02-4T10:30:00+09:00', endAt: '2024-02-4T11:30:00+09:00' }],
+            },
+            {
+                dayOfWeek: 'thu',
+                schedule: [{ startAt: '2024-02-4T10:30:00+09:00', endAt: '2024-02-4T11:30:00+09:00' }],
+            },
+            {
+                dayOfWeek: 'fri',
+                schedule: [{ startAt: '2024-02-4T10:30:00+09:00', endAt: '2024-02-4T11:30:00+09:00' }],
+            },
+        ],
+    },
+];
 
 export const handlers = [
-    http.get('/api/v1/oauth2/status', () => {
+    //Member//
+    //회원 전체 조회
+    http.get('/v1/members/all', () => {
+        return HttpResponse.json({ List });
+    }),
+    //회원 정보 조회
+    http.get('/v1/members/:memberId', (request) => {
+        const MemberId = request.params.memberId;
+        const MemberResponse = List.find((member) => member.memberId === parseInt(MemberId));
+        if (MemberResponse) {
+            return HttpResponse.json(MemberResponse);
+        }
+    }),
+    //상태메시지 설정
+    http.put('/v1/members/:status', (request) => {
+        const Status = request.params.status;
         return HttpResponse.json({
             commonResponse: 'SUCCESS',
-            message: '정보 확인',
-            data: token,
+            message: '상태메세지 설정 성공',
+            data: Status,
         });
     }),
-    http.post('/api/v1/oauth2/login', () => {
-        const body = {
-            email: 'use@example.com',
-            password: 'password123',
-        };
+    //닉네임 재설정
+    http.put('/v1/projects/members/nickname', (request) => {
+        const { projectId, nickname } = request.body;
         return HttpResponse.json({
-            data: token,
+            commonResponse: 'SUCCESS',
+            message: '닉네임 재설정 성공',
+            data: nickname + '으로 변경 완료',
         });
     }),
-    http.get('/api/v1/users/all', () => {
-        return HttpResponse.json({});
+    //프로젝트 보관
+    http.post('/v1/members/storage/:projectId', () => {
+        return HttpResponse.json({
+            commonResponse: 'SUCCESS',
+            message: '프로젝트 보관 성공',
+            data: null,
+        });
+    }),
+    //프로젝트 삭제
+    http.delete('/v1/members/storage/:projectId', () => {
+        return HttpResponse.json({
+            commonResponse: 'SUCCESS',
+            message: '프로젝트 삭제 성공',
+            data: null,
+        });
+    }),
+
+    //Schedule
+    //시간표 전체 조회
+    http.get('/v1/schedules/all', (time) => {
+        const { startdate, enddate } = time.params;
+        const filteredSchedules = ScheduleList.map((schedule) => ({
+            ...schedule,
+            schedule: schedule.schedule.filter((item) => item.startAt >= startdate && item.endAt <= enddate),
+        }));
+        return HttpResponse.json(filteredSchedules);
+    }),
+    //프로젝트 내 모든 사용자 시간표 조회
+    http.get('/v1/projects/:projectId/schedules', () => {
+        const { startdata, enddata } = time.params;
+        const filteredSchedules = ScheduleList.map((schedule) => ({
+            ...schedule,
+            schedule: schedule.schedule.filter((item) => item.startAt >= startdata && item.endAt <= enddata),
+        }));
+        return HttpResponse.json(filteredSchedules);
     }),
 ];
