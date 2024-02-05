643496717f1401a42666b8d66869700ba27b7a93
unknown <gojh1013@gmail.com>
2024-02-05 15:12:01 +0900

    feat: 브랜치 생성

---
+++ src/MainPage/Login.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+const defaultImg = 'C:UsersadminOneDrive - inu.ac.kr그림타임피스 사진들기본이미지.png';
+
+function Login() {
+    const [image, setImage];
+    return <img src="defaultImg" />;
+}
