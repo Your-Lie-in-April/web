4563bf50312ebf790cc30683301b39e70009a4d3
unknown <gojh1013@gmail.com>
2024-02-13 18:42:13 +0900

    style: Info.tsx 컴포넌트

--- src/Pages/ProjectPage/Info.tsx
+++ src/Pages/ProjectPage/Info.tsx
@@ -4,24 +4,56 @@ import styled from 'styled-components';
 const Container = styled.div`
     width: 1920px;
     height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     background-color: #ffffff;
     border: 1px solid #000000;
 `;
 
 const Title = styled.div`
+    margin-top: 20px;
     width: 820px;
     height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     border: 1px solid #000000;
     border-radius: 70px;
+    ::placeholder {
+        color: black;
+    }
+`;
+const TitleText = styled.input`
+    display: flex;
+    width: 765px;
+    padding: 8px;
     font-weight: bold;
     font-size: 32px;
-    text-align: center;
     font-family: Pretendard;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    text-align: center;
+    border: transparent;
+    color: black;
+    outline: none;
+    &:focus {
+        outline: none;
+    }
 `;
 
 const Content = styled.div`
-    width: 820px;
-    height: 34px;
+    display: flex;
+    width: 828px;
+    padding: 8px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+`;
+
+const ContentText = styled.input`
     color: #7d7d7d;
     text-align: center;
     font-family: Pretendard;
@@ -29,6 +61,13 @@ const Content = styled.div`
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    border: transparent;
+    //color: ${(props) => (props.value ? '#000000' : '#7d7d7d')};
+    outline: none;
+
+    &:focus {
+        outline: none;
+    }
 `;
 
 const Make = styled.div`
@@ -37,17 +76,27 @@ const Make = styled.div`
     border-radius: 40px;
     background-color: #ffffff;
     border: 1px solid #000000;
-    font-size: 32px;
-    font-weight: 500;
-    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     font-family: Pretendard;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    margin-bottom: 13px;
+    margin-left: 1150px;
 `;
 
 const Info: FC = () => {
     return (
         <Container>
-            <Title>프로젝트 제목을 작성해주세요</Title>
-            <Content>프로젝트 내용을 작성해주세요</Content>
+            <Title>
+                <TitleText type="text" placeholder="프로젝트 제목을 작성해주세요"></TitleText>
+            </Title>
+            <Content>
+                <ContentText type="text" placeholder="프로젝트 내용을 작성해주세요"></ContentText>
+            </Content>
             <Make>커버만들기</Make>
         </Container>
     );
