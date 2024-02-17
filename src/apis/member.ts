21299f2c18962356ff60602d4ef7bd552f28ba2e
unknown <gojh1013@gmail.com>
2024-02-17 20:46:12 +0900

    feat: react-query (member)

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -1,6 +1,13 @@
 import { API } from '#/constants/api';
 import getAPIResponseData from '#/utils/getAPIResponseData';
-import { MemberInfoResDto, MemberResDto, MemberStateResDto } from '#/Types/member';
+import {
+    MemberInfoResDto,
+    MemberResDto,
+    MemberStateResDto,
+    MemberNicknameReqDto,
+    ProjectStoreResDto,
+    ProjectDeleteResDto,
+} from '#/Types/member';
 
 export const getMemberList = async () => {
     return await getAPIResponseData<MemberResDto>({
@@ -16,9 +23,30 @@ export const getMemberInfo = async () => {
     });
 };
 
-export const putMemberStatus = async (status: string) => {
+export const putMemberStatus = async (state: string) => {
     return await getAPIResponseData<MemberStateResDto>({
         method: 'PUT',
-        url: API.MEMBER_STATUS(status),
+        url: API.MEMBER_STATE(state),
+    });
+};
+
+export const putMemberNickname = async () => {
+    return await getAPIResponseData<MemberNicknameReqDto>({
+        method: 'PUT',
+        url: API.MEMBER_NICKNAME,
+    });
+};
+
+export const postProjectStore = async (projectId: number) => {
+    return await getAPIResponseData<ProjectStoreResDto>({
+        method: 'POST',
+        url: API.MEMBER_STORAGE_PROJECT(projectId),
+    });
+};
+
+export const deleteProjectStore = async (projectId: number) => {
+    return await getAPIResponseData<ProjectDeleteResDto>({
+        method: 'DELETE',
+        url: API.MEMBER_STORAGE_PROJECT(projectId),
     });
 };
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,5 +4,7 @@ type String = string;
 export const API = {
     MEMBER: '/members/all',
     MEMBER_ID: (id?: Id) => `/members/${id ?? ':id'}`,
-    MEMBER_STATUS: (string?: String) => `/members/${string ?? ':string'}`,
+    MEMBER_STATE: (string?: String) => `/members/${string ?? ':string'}`,
+    MEMBER_NICKNAME: '/projects/members/nickname',
+    MEMBER_PROJECTID: (id?: Id) => `/members/storage/${id ?? ':id'}`,
 } as const;
