fae57137851214750e7b4768bc51167d680bf7b9
고재현 <gojh1013@gmail.com>
2024-02-24 17:43:47 +0900

    style: 스타일 조정중

--- src/Pages/ProjectMakePage/projecttime.tsx
+++ src/Pages/ProjectMakePage/projecttime.tsx
@@ -1,59 +1,99 @@
-import { FC, useState, useRef, useEffect } from 'react';
-import ReactDatePicker, { registerLocale, setDefaultLocale } from 'react-datepicker';
+import React, { FC, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
 import styled from 'styled-components';
-import SelectTime from './projectcalendar';
-import './projecttime.css';
 import { FaAngleDown } from 'react-icons/fa';
 import { ko } from 'date-fns/locale/ko';
 
 const ProjectTimeContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 50px;
     width: 548px;
     height: 401px;
     background: #fff;
 `;
 
+const DateContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
 const SDatePicker = styled(ReactDatePicker)`
     margin-top: 12px;
-    width: 200px;
+    width: 208px;
     align-items: center;
     gap: 12px;
-    padding: 8px 20px;
+    padding: 8px;
     font-size: 28px;
     font-weight: 400;
     border-radius: 20px;
     background: #f5f5f5;
     text-align: center;
-    &: hover {
+
+    &:hover {
         cursor: pointer;
     }
 `;
+
+const Text = styled.div`
+    color: #000;
+    font-family: Pretendard;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-bottom: 12x; /* Adjusted margin */
+    margin-left: 8px; /* Adjusted margin */
+`;
+
 const Example: FC = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+
     return (
         <ProjectTimeContainer>
-            {' '}
-            <div style={{ marginLeft: '30px', display: 'flex', flexDirection: 'row' }}>
-                <SDatePicker
-                    locale={ko}
-                    dateFormat={'YYYY-MM-dd'}
-                    selected={startDate}
-                    onChange={(date: Date) => setStartDate(date)}
-                    selectsStart
-                    startDate={startDate}
-                />
-                <div style={{ color: 'white' }}>~</div>
-                <SDatePicker
-                    locale={ko}
-                    dateFormat={'YYYY-MM-dd'}
-                    selected={endDate}
-                    onChange={(date: Date) => setEndDate(date)}
-                    selectsEnd
-                    endDate={endDate}
-                    minDate={startDate}
-                />
+            <div style={{ display: 'flex', marginTop: '42px' }}>
+                <DateContainer>
+                    <Text>프로젝트 시작일</Text>
+                    <SDatePicker
+                        locale={ko}
+                        dateFormat={'YYYY-MM-dd'}
+                        selected={startDate}
+                        onChange={(date: Date) => setStartDate(date)}
+                        startDate={startDate}
+                        onFocus={(e) => e.target.blur()}
+                    />
+                </DateContainer>
+
+                <span
+                    style={{
+                        fontSize: '32px',
+                        marginTop: '35px',
+                        fontWeight: '600',
+                        verticalAlign: 'middle',
+                        textAlign: 'center',
+                    }}
+                >
+                    ~
+                </span>
+
+                <DateContainer>
+                    <Text>프로젝트 종료일</Text>
+                    <SDatePicker
+                        locale={ko}
+                        dateFormat={'YYYY-MM-dd'}
+                        selected={endDate}
+                        onChange={(date: Date) => setEndDate(date)}
+                        selectsEnd
+                        endDate={endDate}
+                        minDate={startDate}
+                        onFocus={(e) => e.target.blur()}
+                    />
+                </DateContainer>
             </div>
         </ProjectTimeContainer>
     );
 };
+
 export default Example;
