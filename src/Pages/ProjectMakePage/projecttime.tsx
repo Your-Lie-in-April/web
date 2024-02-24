237118e2c5bf077210b4ea3c13d0033f1d27710f
unknown <gojh1013@gmail.com>
2024-02-24 22:56:28 +0900

    style: 요일 생성 & 시간표 시간

--- src/Pages/ProjectMakePage/projecttime.tsx
+++ src/Pages/ProjectMakePage/projecttime.tsx
@@ -35,6 +35,23 @@ const SDatePicker = styled(ReactDatePicker)`
     }
 `;
 
+const STimePicker = styled.div`
+    margin-top: 12px;
+    width: 166px;
+    height: 33px;
+    padding: 4px;
+    font-size: 28px;
+    font-weight: 400;
+    border-radius: 20px;
+    background: #f5f5f5;
+    text-align: center;
+    margin: auto;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
 const Text = styled.div`
     color: #000;
     font-family: Pretendard;
@@ -52,21 +69,40 @@ const Separator = styled.span`
     font-weight: 600;
     text-align: center;
     margin-top: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const MakeWeekend = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    gap: 8px;
     margin-top: 40px;
 `;
-
+const WeekendContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    margin-left: 21px;
+    justify-content: center;
+    margin-top: 8px;
+`;
 const Weekend = styled.div`
     width: 60px;
     height: 60px;
     border-radius: 5px;
     background: #d9d9d9;
+    font-size: 32px;
+    font-weight: bold;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &: hover {
+        cursor: pointer;
+        background: #b79fff;
+    }
 `;
 
 const ProjectTime: FC = () => {
@@ -111,9 +147,56 @@ const ProjectTime: FC = () => {
                     />
                 </DateContainer>
             </div>
+
             <MakeWeekend>
                 <Text>생성할 요일</Text>
+                <WeekendContainer>
+                    <Weekend>S</Weekend>
+                    <Weekend>M</Weekend>
+                    <Weekend>T</Weekend>
+                    <Weekend>W</Weekend>
+                    <Weekend>T</Weekend>
+                    <Weekend>F</Weekend>
+                    <Weekend>S</Weekend>
+                </WeekendContainer>
             </MakeWeekend>
+
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    marginTop: '42px',
+                }}
+            >
+                <DateContainer>
+                    <Text>시간표 시작시간</Text>
+                    <SDatePicker
+                        locale={ko}
+                        dateFormat={'YYYY-MM-dd'}
+                        selected={startDate}
+                        onChange={(date: Date) => setStartDate(date)}
+                        startDate={startDate}
+                        onFocus={(e) => e.target.blur()}
+                    />
+                </DateContainer>
+
+                <Separator>~</Separator>
+
+                <DateContainer>
+                    <Text>시간표 종료시간</Text>
+                    <SDatePicker
+                        locale={ko}
+                        dateFormat={'YYYY-MM-dd'}
+                        selected={endDate}
+                        onChange={(date: Date) => setEndDate(date)}
+                        selectsEnd
+                        endDate={endDate}
+                        minDate={startDate}
+                        onFocus={(e) => e.target.blur()}
+                    />
+                </DateContainer>
+            </div>
         </ProjectTimeContainer>
     );
 };
