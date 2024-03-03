4fe33125a4ccd7a10602840451fd5cc9dcf76a2c
unknown <gojh1013@gmail.com>
2024-03-03 14:25:33 +0900

    style: 제목작성&컨텐트부분 원상복귀

--- src/Pages/ProjectMakePage/Info.tsx
+++ src/Pages/ProjectMakePage/Info.tsx
@@ -3,15 +3,9 @@ import styled from 'styled-components';
 import Cover from './cover';
 
 interface ContentTextProps {
-    isTitleKeyPress: boolean;
     focused: boolean;
 }
-interface TitleProps {
-    keypress: boolean;
-}
-interface MakeProps {
-    isTitleKeyPress: boolean;
-}
+
 const Container = styled.div`
     width: 1920px;
     height: 200px;
@@ -23,7 +17,6 @@ const Container = styled.div`
     border: 1px solid #000000;
     margin-top: 16px;
     margin-bottom: 16px;
-    position: relative;
 `;
 const MakeContainer = styled.div`
     width: 1043px;
@@ -36,15 +29,14 @@ const TitleContainer = styled.div`
     width: 820px;
     heigth: 136px;
 `;
-const Title = styled.div<TitleProps>`
+const Title = styled.div`
     width: 820px;
     height: 70px;
     display: flex;
     align-items: center;
     justify-content: center;
-    border: ${({ keypress }) => (keypress ? '1px solid transparent' : '1px solid #000000')};
+    border: 1px solid #000000;
     border-radius: 70px;
-    background-color: ${({ keypress }) => (keypress ? '#ffffff' : 'transparent')};
     ::placeholder {
         color: black;
     }
@@ -66,7 +58,6 @@ const TitleText = styled.input`
     &:focus {
         outline: none;
     }
-    background-color: transparent;
 `;
 
 const Content = styled.div`
@@ -79,7 +70,8 @@ const Content = styled.div`
 `;
 
 const ContentText = styled.textarea<ContentTextProps>`
-    color: ${({ isTitleKeyPress, focused }) => (isTitleKeyPress || focused ? '#000000' : '#7d7d7d')};
+    color: ${({ focused }) => (focused ? '#000000' : '#7d7d7d')};
+    color: #7d7d7d;
     text-align: center;
     font-family: Pretendard;
     font-size: 28px;
@@ -94,23 +86,19 @@ const ContentText = styled.textarea<ContentTextProps>`
         outline: none;
         color: #000000;
     }
-    &::placeholder {
-        color: ${({ isTitleKeyPress }) => (isTitleKeyPress ? '#000000' : '#your_default_color')};
-    }
     overflow: hidden;
 `;
 
-const Make = styled.button<MakeProps>`
+const Make = styled.button`
     width: 145px;
     height: 35px;
     border-radius: 40px;
-    color: ${({ isTitleKeyPress }) => (isTitleKeyPress ? 'white' : 'black')};
-    background-color: ${({ isTitleKeyPress }) => (isTitleKeyPress ? '#633ae2' : '#ffffff')};
+    background-color: #ffffff;
     border: 1px solid #000000;
     display: flex;
     font-family: Pretendard;
     white-space: nowrap;
-    font-size: ${({ isTitleKeyPress }) => (isTitleKeyPress ? '14px' : '22px')};
+    font-size: 22px;
     font-style: normal;
     font-weight: 600;
     line-height: normal;
@@ -129,14 +117,12 @@ const CoverContainer = styled.div`
     transform: translateX(-50%);
     z-index: 5;
 `;
+
 const Info: FC = () => {
     const [content, setContent] = useState<string>('');
-    const [keypress, setIsKeypress] = useState<boolean>(false);
     const [isTitleClicked, setIsTitleClicked] = useState<boolean>(false);
     const [isContentClicked, setIsContentClicked] = useState<boolean>(false);
-    const [isTitleKeyPress, setIsTitleKeyPress] = useState<boolean>(false);
     const [isCoverClicked, setIsCoverClicked] = useState<boolean>(false);
-
     const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         const inputText = e.target.value;
         const limitedText = inputText.slice(0, 58);
@@ -145,18 +131,6 @@ const Info: FC = () => {
             setContent(limitedText);
         }
     };
-    const handleTitleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            setIsKeypress(true);
-            setIsTitleKeyPress(true);
-            e.currentTarget.blur();
-        }
-    };
-
-    const handleTitleFocus = () => {
-        setIsTitleKeyPress(false);
-    };
-
     const toggleCover = () => {
         setIsCoverClicked((prevIsCoverClicked) => !prevIsCoverClicked);
     };
@@ -165,26 +139,16 @@ const Info: FC = () => {
         <Container>
             <MakeContainer>
                 <TitleContainer>
-                    <Title keypress={keypress}>
+                    <Title>
                         <TitleText
                             type="text"
-                            onFocus={() => {
-                                setIsTitleClicked(true);
-                                setIsKeypress(false);
-                                handleTitleFocus();
-                                setIsCoverClicked(false);
-                            }}
-                            onBlur={() => {
-                                setIsTitleClicked(false);
-                                setIsKeypress(true);
-                            }}
+                            onFocus={() => setIsTitleClicked(true)}
+                            onBlur={() => setIsTitleClicked(false)}
                             placeholder={isTitleClicked === true ? '' : '프로젝트 제목을 작성해주세요'}
-                            onKeyDown={handleTitleKeyPress}
                         ></TitleText>
                     </Title>
                     <Content>
                         <ContentText
-                            isTitleKeyPress={isTitleKeyPress}
                             value={content}
                             focused
                             onChange={handleTextareaChange}
@@ -194,13 +158,7 @@ const Info: FC = () => {
                         />
                     </Content>
                 </TitleContainer>
-                {isTitleKeyPress ? (
-                    <Make isTitleKeyPress={isTitleKeyPress}>프로젝트 보관하기</Make>
-                ) : (
-                    <Make isTitleKeyPress={isTitleKeyPress} onClick={toggleCover}>
-                        커버 만들기
-                    </Make>
-                )}
+                <Make onClick={toggleCover}>커버 만들기</Make>
             </MakeContainer>
             {isCoverClicked && (
                 <CoverContainer>
