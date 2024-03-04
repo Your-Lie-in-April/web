a28fa6c28ad9f6ad80ff1cc43effdad70e36c8b5
ekkk1126 <cabi0203@naver.com>
2024-03-04 14:58:55 +0900

    style : 타임피스-로그인 배너 위치 조정

--- src/Pages/Layouts/AfterLogin.tsx
+++ src/Pages/Layouts/AfterLogin.tsx
@@ -1,39 +1,38 @@
-import { FC } from 'react';
-import styled from 'styled-components';
+import { FC } from "react";
+import styled from "styled-components";
 
 const AfterLoginDiv = styled.div`
-    width: 100%;
-    height: 100px;
-    background-color: #f1f1f1;
-    display: flex;
-    align-items: center;
+  width: 100%;
+  height: 100px;
+  background-color: #f1f1f1;
+  display: flex;
+  align-items: center;
+  position: relative;
 `;
 const Logo = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 700;
-    width: 173px;
-    font-size: 32px;
-    margin-top: 49px;
-    margin: auto;
-    margin-bottom: 13px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  margin: auto;
+  margin-bottom: 14px;
 `;
 const LogIn = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 22px;
-    margin-right: 319.41px;
-    margin-top: 55px;
-    margin-bottom: 19px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 22px;
+  position: absolute;
+  right: 319.5px;
+  bottom: 20px;
 `;
 
 const AfterLogin: FC = () => {
-    return (
-        <AfterLoginDiv>
-            <Logo>TIME PIECE</Logo>
-            <LogIn>My page</LogIn>
-        </AfterLoginDiv>
-    );
+  return (
+    <AfterLoginDiv>
+      <Logo>TIME PIECE</Logo>
+      <LogIn>My page</LogIn>
+    </AfterLoginDiv>
+  );
 };
 export default AfterLogin;
