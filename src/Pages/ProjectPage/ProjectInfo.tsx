3cfe50aa0d88548f2bbef4db15f3a25885b75184
ekkk1126 <cabi0203@naver.com>
2024-03-09 18:19:49 +0900

    style : 프로젝트인포배너 수정(버튼추가)

--- src/Pages/ProjectPage/ProjectInfo.tsx
+++ src/Pages/ProjectPage/ProjectInfo.tsx
@@ -1,16 +1,25 @@
 import styled from "styled-components";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 
 const Container = styled.div`
-  display: flex;
+  position: relative;
   width: 100%;
   height: 200px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 32px;
-  padding: 16px 320px;
   background: #d9d9d9;
   box-sizing: border-box;
+  padding-top: 28px;
+`;
+
+const ProjectInfoDiv = styled.div`
+  width: 900px;
+  height: 172px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 32px;
+  margin: auto;
 `;
 
 const CommonText = styled.div`
@@ -31,14 +40,62 @@ const MainContainer = styled.div`
   box-sizing: border-box;
 `;
 
+const SettingDiv = styled.div`
+  position: absolute;
+  bottom: 16px;
+  right: 319px;
+  width: 221px;
+  height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  border-radius: 20px;
+  background: #633ae2;
+  box-sizing: border-box;
+`;
+
+const SettingBtn = styled.button`
+  color: #ffffff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+
+  display: flex;
+  gap: 4px;
+
+  border-radius: 20px;
+  padding: 0;
+  margin: 0;
+  background: transparent;
+  box-sizing: border-box;
+`;
+
 const ProjectInfo = () => {
   return (
-    <Container>
-      <CommonText style={{ fontSize: "42px", fontWeight: "700" }}>
-        프로젝트 제목
-      </CommonText>
-      <CommonText>프로젝트 내용</CommonText>
-    </Container>
+    <>
+      <Container>
+        <ProjectInfoDiv>
+          <CommonText style={{ fontSize: "42px", fontWeight: "700" }}>
+            프로젝트 제목
+          </CommonText>
+          <CommonText>프로젝트 내용</CommonText>
+        </ProjectInfoDiv>
+        <SettingDiv>
+          <SettingBtn>
+            <BorderColorOutlinedIcon style={{ fontSize: "18px" }} />
+            커버 수정
+          </SettingBtn>
+          <SettingBtn>
+            <InboxOutlinedIcon style={{ fontSize: "18px" }} />
+            프로젝트 보관
+          </SettingBtn>
+        </SettingDiv>
+      </Container>
+    </>
   );
 };
 
