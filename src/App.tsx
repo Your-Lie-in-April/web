31010c0d9abfa83d8a8735843c3407deca1cb622
ekkk1126 <cabi0203@naver.com>
2024-03-10 02:32:04 +0900

    style : 폰트 파일 임포트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import styled from "styled-components";
-import Info from "./Pages/ProjectMakePage/Info";
-import MainPage from "./Pages/MainPage/MainPage";
-import ProjectMakePage from "./Pages/ProjectMakePage/projectmakepage";
-import { Hidden } from "@mui/material";
+import styled from 'styled-components';
+import Info from './Pages/ProjectMakePage/Info';
+import MainPage from './Pages/MainPage/MainPage';
+import ProjectMakePage from './Pages/ProjectMakePage/projectmakepage';
+import { Hidden } from '@mui/material';
+import './App.css';
 
 function App() {
   return (
