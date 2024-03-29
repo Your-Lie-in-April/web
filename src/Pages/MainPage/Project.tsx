69cc31bda76701ea84501b9b212b05f649de2f42
ekkk1126 <cabi0203@naver.com>
2024-03-30 03:01:32 +0900

    style : 메인페이지 프로젝트 스타일 수정

--- src/Pages/MainPage/Project.tsx
+++ src/Pages/MainPage/Project.tsx
@@ -1,8 +1,10 @@
-import styled from "styled-components";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined";
-import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
-import { useState } from "react";
+import styled from 'styled-components';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { useState } from 'react';
+import LeaveProject from '../Modal/LeaveProject';
 
 interface MoreTextProps extends React.HTMLAttributes<HTMLDivElement> {
   isMove?: boolean;
@@ -18,6 +20,7 @@ const ProjectBox = styled.div`
   border-radius: 16px;
   display: flex;
   color: #ffffff;
+  position: relative;
 `;
 
 const TextBox = styled.div`
@@ -32,29 +35,34 @@ const TextBox = styled.div`
   box-sizing: border-box;
   align-items: flex-start;
   gap: 8px;
-`;
 
-const ProjectName = styled.text`
   color: #000000;
-  font-family: "Pretendard";
-  font-size: 24px;
+  font-family: 'Pretendard';
   font-style: normal;
-  font-weight: 700;
   line-height: normal;
   text-transform: uppercase;
+`;
+
+const ProjectName = styled.div`
+  width: 100%;
+  font-size: 24px;
+  font-weight: 700;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const DetailText = styled.div`
-  color: #000000;
-  font-family: "Pretendard";
   font-size: 16px;
-  font-style: normal;
   font-weight: 400;
-  line-height: normal;
-  text-transform: uppercase;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
 `;
 
-const MoreButton = styled.button`
+const StyledButton = styled.button`
   background: none;
   border: none;
   padding: 0;
@@ -82,7 +90,6 @@ const MoreDiv = styled.div`
 const MoreBox = styled.div`
   display: flex;
   flex-direction: row;
-  width: px;
   height: 36px;
   border-radius: 6px;
   background-color: #ffffff;
@@ -110,58 +117,82 @@ const MoreItem = styled.button`
 `;
 
 const MoreText = styled.div<MoreTextProps>`
-  font-family: "Pretendard";
+  font-family: 'Pretendard';
   font-weight: 400;
   font-size: 10px;
   font-style: normal;
   line-height: normal;
   text-transform: uppercase;
-  letter-spacing: ${({ isMove }) => (isMove ? "-1.2px" : "normal")};
+  letter-spacing: ${({ isMove }) => (isMove ? '-1.2px' : 'normal')};
+`;
+
+const CancelBtn = styled(CancelIcon)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  color: #ffffff;
+  width: 36px;
+  height: 36px;
 `;
 
 const Project = () => {
-  const [showMore, setShowMore] = useState(false);
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const [isCancleBtn, setIsCancelBtn] = useState<boolean>(false);
+
   const toggleMoreBtn = () => {
     setShowMore(!showMore);
   };
 
-  return (
-    <ProjectBox>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "4px",
-        }}
-      >
-        <MoreDiv>
-          {showMore && (
-            <MoreBox>
-              <MoreItem>
-                <PushPinOutlinedIcon sx={{ fontSize: 18 }} />
-                <MoreText>상단고정</MoreText>
-              </MoreItem>
+  const onClickCancelBtn = () => {
+    setIsCancelBtn(!isCancleBtn);
+  };
 
-              <MoreItem>
-                <InboxOutlinedIcon sx={{ fontSize: 18 }} />
-                <MoreText isMove>보관함이동</MoreText>
-              </MoreItem>
-            </MoreBox>
-          )}
-          <MoreButton>
-            <StyledMoreBtn sx={{ fontSize: 32 }} onClick={toggleMoreBtn} />
-          </MoreButton>
-        </MoreDiv>
-        <TextBox>
-          <ProjectName>2023 여름방학 프로젝트</ProjectName>
-          <DetailText>
-            2023 앱센터 프로젝트 입니다.
-            <br />
-            2023 앱센터 프로젝트 입니다.
-          </DetailText>
-        </TextBox>
-      </div>
-    </ProjectBox>
+  return (
+    <>
+      <ProjectBox>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '4px',
+          }}
+        >
+          <MoreDiv>
+            {showMore && (
+              <>
+                <StyledButton>
+                  <CancelBtn onClick={onClickCancelBtn} sx={{ fontSize: 36 }} />
+                </StyledButton>
+                <MoreBox>
+                  <MoreItem>
+                    <PushPinOutlinedIcon sx={{ fontSize: 18 }} />
+                    <MoreText>상단고정</MoreText>
+                  </MoreItem>
+
+                  <MoreItem>
+                    <InboxOutlinedIcon sx={{ fontSize: 18 }} />
+                    <MoreText isMove>보관함이동</MoreText>
+                  </MoreItem>
+                </MoreBox>
+              </>
+            )}
+            <StyledButton>
+              <StyledMoreBtn sx={{ fontSize: 32 }} onClick={toggleMoreBtn} />
+            </StyledButton>
+          </MoreDiv>
+          <TextBox>
+            <ProjectName>
+              2023 여름방학 프로젝트2023 여름방학 프로젝트2023 여름방학 프로젝트
+            </ProjectName>
+            <DetailText>
+              2023 앱센터 프로젝트 입니다.2023 앱센터 프로젝트 입니다.2023
+              앱센터 프로젝트 입니다.2023 앱센터 프로젝트 입니다.
+            </DetailText>
+          </TextBox>
+        </div>
+      </ProjectBox>
+      {isCancleBtn && <LeaveProject onClose={onClickCancelBtn} />}
+    </>
   );
 };
 
