67dbeafb0e966954329e16a78935dc502a29bc69
ekkk1126 <cabi0203@naver.com>
2024-03-30 02:20:42 +0900

    style : 로그인배너 및 메인 페이지 전체 간격 수정

--- src/Pages/Layouts/BeforeLogin.tsx
+++ src/Pages/Layouts/BeforeLogin.tsx
@@ -2,41 +2,47 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 const BeforeLoginDiv = styled.div`
-    width: 1920px;
-    height: 100px;
-    background-color: #f1f1f1;
-    display: flex;
-    align-items: center;
+  width: 100vw;
+  height: 100px;
+  background-color: #f1f1f1;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  z-index: 1;
+
+  color: #000000;
+  font-style: normal;
 `;
 
 const Logo = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 700;
-    width: 173px;
-    font-size: 32px;
-    line-height: 38px;
-    margin-top: 49px;
-    margin-left: 873.5px;
-    margin-bottom: 13px;
+  position: relative;
+  bottom: 14px;
+  justify-content: center;
+  align-item: center;
+
+  white-space: nowrap;
+
+  font-weight: 700;
+  font-size: 32px;
 `;
+
 const LogIn = styled.div`
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 550;
-    font-size: 22px;
-    margin-top: 55px;
-    margin-bottom: 21px;
+  position: relative;
+  bottom: 21px;
+  justify-content: flex-end;
+  flex-basis: 20%;
+  white-space: nowrap;
+  font-size: 22px;
 `;
 
 const BeforeLogin: FC = () => {
-    return (
-        <BeforeLoginDiv>
-            <Logo>TIME PIECE</Logo>
-            <div style={{ marginLeft: '493.17px' }}>
-                <LogIn>Log in</LogIn>
-            </div>
-        </BeforeLoginDiv>
-    );
+  return (
+    <BeforeLoginDiv>
+      <div style={{ justifyContent: 'flex-start', flexBasis: '20%' }} />
+      <Logo>TIME PIECE</Logo>
+      <LogIn>Log in</LogIn>
+    </BeforeLoginDiv>
+  );
 };
 export default BeforeLogin;
