f31a3793c47c1a774da5b4d45e5e4047ef9b0afa
ekkk1126 <cabi0203@naver.com>
2024-04-03 00:42:00 +0900

    style : 프로젝트 보관함 페이지 전체 간격 조정

--- src/Pages/StoragePage/StoragePage.tsx
+++ src/Pages/StoragePage/StoragePage.tsx
@@ -8,12 +8,13 @@ import SpringIcon from './Icon/SpringIcon';
 import GraphicIcons from './Icon/GraphicIcons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    min-height : 1200px;
-    margin: 0;
-    background-color: #212121;
-    -ms-overflow-style: none;
-  }
+body {
+  width : 100%;
+  min-width : 1366px;
+  margin: 0 auto;
+  background-color: #212121;
+  -ms-overflow-style: none;
+}
 
   ::-webkit-scrollbar {
     display: none;
@@ -21,65 +22,67 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Title = styled.div`
-  color: #ffffff;
-  text-align: center;
-  font-size: 42px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
+    color: #ffffff;
+    text-align: center;
+    font-size: 42px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
 `;
 
 const SearchField = styled.input`
-  width: 785px;
-  height: 69px;
-  border-radius: 40px;
-  background: #ffffff;
-  color: #a4a4a4;
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 800;
-  line-height: normal;
-  padding: 18px 28px;
-  box-sizing: border-box;
-  border: none;
-  outline: none;
-  z-index: 0;
+    width: 785px;
+    height: 69px;
+    border-radius: 40px;
+    background: #ffffff;
+    color: #a4a4a4;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+    padding: 18px 28px;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+    z-index: 0;
 `;
 
 const StoragePage = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <GraphicIcons />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '130px',
-          width: '100vw',
-          margin: '0 auto',
-        }}
-      >
-        <AfterLogin />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '49px',
-            alignItems: 'center',
-          }}
-        >
-          <div
-            style={{ display: 'flex', flexDirection: 'column', gap: '48px' }}
-          >
-            <Title>프로젝트 보관함</Title>
-            <SearchField placeholder="프로젝트 검색" />
-          </div>
-          <StorageProjectList />
-        </div>
-      </div>
-      <div style={{ width: '100vw', height: '172px' }}></div>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <GraphicIcons />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '130px',
+                }}
+            >
+                <AfterLogin />
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '49px',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '48px',
+                        }}
+                    >
+                        <Title>프로젝트 보관함</Title>
+                        <SearchField placeholder="프로젝트 검색" />
+                    </div>
+                    <StorageProjectList />
+                </div>
+            </div>
+            <div style={{ width: '100vw', height: '172px' }}></div>
+        </>
+    );
 };
 export default StoragePage;
