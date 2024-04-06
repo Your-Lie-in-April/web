bb8ac90e108ed5194ad5ca435057982623b43407
ekkk1126 <cabi0203@naver.com>
2024-04-06 19:44:33 +0900

    feat : 내 시간표 수정- 마우스 드래그 이벤트 수정

--- src/Pages/ProjectPage/Schedule/EditMyTime.tsx
+++ src/Pages/ProjectPage/Schedule/EditMyTime.tsx
@@ -3,172 +3,197 @@ import styled from 'styled-components';
 import { TimeSlotLeft, TimeSlotRight } from './EditMyTimeCircle';
 
 const CommonText = styled.div`
-  color: #000000;
-  text-align: center;
-  font-family: Pretendard;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
+    color: #000000;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `;
 
 const TimeTableDiv = styled.div`
-  width: 100%;
-  height: 325.17px;
-  display: flex;
-  flex-direction: column;
+    width: 100%;
+    height: 325.17px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const TimeList = styled.div`
-  width: 100%;
-  height: 325.17px;
-  display: flex;
-  justify-content: center;
+    width: 100%;
+    height: 325.17px;
+    display: flex;
+    justify-content: center;
 `;
 
 const DayTextList = styled.div`
-  height: 290.76px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-  margin-top: 10px;
+    height: 290.76px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    margin-top: 10px;
 `;
 
 const HourTextList = styled.div`
-  display: flex;
-  gap: 14px;
-  align-items: center;
-  justify-content: flex-end;
-  margin-right: 55px;
+    display: flex;
+    gap: 14px;
+    align-items: center;
+    justify-content: flex-end;
+    margin-right: 55px;
 `;
 
 const DayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 const HoursOfDay = [...Array(15).keys()].map((_, index) => index + 9);
 
-const EditMyTime: React.FC = () => {
-  let [startSlot, setStartSlot] = useState<number | null>(null);
-  let [endSlot, setEndSlot] = useState<number | null>(null);
-  let [isMouseDown, setIsMouseDown] = useState<boolean>(false);
-  let [selection, setSelection] = useState<number[]>([]);
+const EditMyTime: React.FC = (props) => {
+    // 내 기존 시간표를 불러옴
+    // let myOriginTime = props.myOriginTime;
 
-  // selection 확인
-  // useEffect(() => {
-  //   console.log(selection);
-  // }, [selection]);
+    const [firstClickSlot, setFirstClickSlot] = useState<number>(0);
+    const [firstClickSlotState, setFirstClickSlotState] =
+        useState<boolean>(false);
+    const [selection, setSelection] = useState<Set<number> | null>(null);
+    const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
 
-  const handleSelectFromHere = (id: number) => {
-    setIsMouseDown(true);
-    setStartSlot(id);
-    setEndSlot(id);
-    setSelection((prevSelection) => {
-      if (!prevSelection.includes(id)) {
-        return [...prevSelection, id];
-      } else {
-        return prevSelection;
-      }
-    });
-  };
+    // 셀 마우스 클릭시
+    const handleMouseClick = (id: number) => {
+        setFirstClickSlot(id);
 
-  const handleSelectToHere = (id: number) => {
-    if (isMouseDown && startSlot) {
-      const currentIndex = id;
-      const startIndex = startSlot;
-      const endIndex = endSlot || 0;
+        if (selection === null) {
+            setSelection(new Set([id]));
+            setFirstClickSlotState(true);
+        } else {
+            if (selection.has(id)) {
+                setFirstClickSlotState(false);
+                const newSelection = new Set(selection);
+                newSelection.delete(id);
+                setSelection(newSelection);
+            } else {
+                setFirstClickSlotState(true);
+                setSelection((prevSelection) => new Set(prevSelection).add(id));
+            }
+        }
 
-      if (currentIndex > startIndex) {
-        setEndSlot(id);
-      } else {
-        setStartSlot(id);
-      }
+        setIsMouseDown(true);
+    };
 
-      const newSelectedSlots: number[] = [];
+    // 마우스가 셀 안에 들어올 경우 (드래그시)
+    const handleMouseEnter = (id: number) => {
+        if (isMouseDown) {
+            if (firstClickSlotState) {
+                setSelection((prevSelection) => new Set(prevSelection).add(id));
+            } else {
+                if (selection !== null) {
+                    const newSelection = new Set(selection);
+                    newSelection.delete(id);
+                    setSelection(newSelection);
+                }
+            }
+        }
+    };
 
-      for (let i = Math.min(startIndex, id); i <= Math.max(endIndex, id); i++) {
-        newSelectedSlots.push(i);
-      }
+    const handleMouseUp = () => {
+        setIsMouseDown(false);
+    };
 
-      setSelection((prevSelection) => {
-        const updatedSelection = new Set([
-          ...prevSelection,
-          ...newSelectedSlots,
-        ]);
-        return Array.from(updatedSelection);
-      });
-    }
-  };
+    const isSlotSelected = (id: number) => {
+        if (selection !== null) {
+            return selection.has(id);
+        }
+    };
 
-  const handleMouseUp = () => {
-    setIsMouseDown(false);
-  };
+    useEffect(() => {
+        if (selection !== null) {
+            setFirstClickSlot(0);
+        }
+    }, [selection]);
 
-  const isSlotSelected = (id: number) => {
-    return selection.includes(id);
-  };
+    return (
+        <TimeTableDiv>
+            <HourTextList>
+                {HoursOfDay.map((h, idx) => (
+                    <CommonText key={idx} style={{ width: '38.45px' }}>
+                        {h}
+                    </CommonText>
+                ))}
+            </HourTextList>
+            <TimeList>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <DayTextList>
+                        {DayOfWeek.map((day, idx) => (
+                            <CommonText
+                                key={idx}
+                                style={{ lineHeight: '32.11px' }}
+                            >
+                                {day}
+                            </CommonText>
+                        ))}
+                    </DayTextList>
+                    <div
+                        onMouseUp={handleMouseUp}
+                        style={{
+                            height: '306.17px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '2px',
+                            padding: '8px',
+                        }}
+                    >
+                        {DayOfWeek.map((day, idx) => (
+                            <div
+                                key={idx}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                }}
+                            >
+                                {HoursOfDay.map((_, hourIdx) => {
+                                    const slotIndex = idx * 15 + hourIdx;
+                                    const slotIdLeft = slotIndex * 2 + 1;
+                                    const slotIdRight = slotIndex * 2 + 2;
 
-  const removeSlotFromSelection = (id: number) => {
-    setSelection(selection.filter((slotId) => slotId !== id));
-  };
-
-  return (
-    <TimeTableDiv>
-      <HourTextList>
-        {HoursOfDay.map((h, idx) => (
-          <CommonText key={idx} style={{ width: '38.45px' }}>
-            {h}
-          </CommonText>
-        ))}
-      </HourTextList>
-      <TimeList>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <DayTextList>
-            {DayOfWeek.map((day, idx) => (
-              <CommonText key={idx} style={{ lineHeight: '32.11px' }}>
-                {day}
-              </CommonText>
-            ))}
-          </DayTextList>
-          <div
-            onMouseUp={handleMouseUp}
-            style={{
-              height: '306.17px',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '2px',
-              padding: '8px',
-            }}
-          >
-            {DayOfWeek.map((day, idx) => (
-              <div key={idx} style={{ display: 'flex', flexDirection: 'row' }}>
-                {HoursOfDay.map((_, hourIdx) => {
-                  const slotIndex = idx * 15 + hourIdx;
-                  const slotIdLeft = slotIndex * 2 + 1;
-                  const slotIdRight = slotIndex * 2 + 2;
-
-                  return (
-                    <React.Fragment key={hourIdx}>
-                      <TimeSlotLeft
-                        id={slotIdLeft}
-                        onSelectFromHere={handleSelectFromHere}
-                        onSelectToHere={handleSelectToHere}
-                        isSelected={isSlotSelected(slotIdLeft)}
-                      />
-                      <TimeSlotRight
-                        id={slotIdRight}
-                        onSelectFromHere={handleSelectFromHere}
-                        onSelectToHere={handleSelectToHere}
-                        isSelected={isSlotSelected(slotIdRight)}
-                      />
-                    </React.Fragment>
-                  );
-                })}
-              </div>
-            ))}
-          </div>
-        </div>
-      </TimeList>
-    </TimeTableDiv>
-  );
+                                    return (
+                                        <React.Fragment key={hourIdx}>
+                                            <TimeSlotLeft
+                                                id={slotIdLeft}
+                                                onSelectSlot={handleMouseClick}
+                                                onMouseEnter={() =>
+                                                    handleMouseEnter(slotIdLeft)
+                                                }
+                                                isSelected={
+                                                    isSlotSelected(
+                                                        slotIdLeft
+                                                    ) ||
+                                                    firstClickSlot == slotIdLeft
+                                                }
+                                            />
+                                            <TimeSlotRight
+                                                id={slotIdRight}
+                                                onSelectSlot={handleMouseClick}
+                                                onMouseEnter={() =>
+                                                    handleMouseEnter(
+                                                        slotIdRight
+                                                    )
+                                                }
+                                                isSelected={
+                                                    isSlotSelected(
+                                                        slotIdRight
+                                                    ) ||
+                                                    firstClickSlot ===
+                                                        slotIdRight
+                                                }
+                                            />
+                                        </React.Fragment>
+                                    );
+                                })}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </TimeList>
+        </TimeTableDiv>
+    );
 };
 
 export default EditMyTime;
--- src/Pages/ProjectPage/Schedule/EditMyTimeCircle.tsx
+++ src/Pages/ProjectPage/Schedule/EditMyTimeCircle.tsx
@@ -2,78 +2,77 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 interface TimeSlotProps {
-  id: number;
-  onSelectFromHere: (id: number) => void;
-  onSelectToHere: (id: number) => void;
-  isSelected: boolean;
+    id: number;
+    isSelected: boolean;
+    onSelectSlot: (id: number) => void;
+    onMouseEnter: () => void;
 }
 
 export const TimeSlotLeft: React.FC<TimeSlotProps> = ({
-  id,
-  onSelectFromHere,
-  onSelectToHere,
-  isSelected,
+    id,
+    isSelected,
+    onSelectSlot,
+    onMouseEnter,
 }) => {
-  const handleMouseDown = () => {
-    onSelectFromHere(id);
-  };
+    const handleMouseDown = () => {
+        onSelectSlot(id);
+    };
 
-  const handleMouseOver = () => {
-    onSelectToHere(id);
-  };
-
-  return (
-    <div
-      id={`Time${id}`}
-      style={{
-        verticalAlign: 'top',
-        display: 'flex',
-        flex: '1',
-        width: '26.665px',
-        height: '39.74px',
-        borderRadius: '20px 0 0 20px',
-        borderTop: '0.5px solid #a4a4a4',
-        borderLeft: '0.5px solid #a4a4a4',
-        borderBottom: '0.5px solid #a4a4a4',
-        background: isSelected ? '#633AE2' : '#D9D9D9',
-        boxSizing: 'border-box',
-      }}
-      onMouseDown={handleMouseDown}
-      onMouseOver={handleMouseOver}
-    ></div>
-  );
+    const handleMouseEnter = () => {
+        onMouseEnter();
+    };
+    return (
+        <div
+            id={`Time${id}`}
+            style={{
+                verticalAlign: 'top',
+                display: 'flex',
+                flex: '1',
+                width: '26.665px',
+                height: '39.74px',
+                borderRadius: '20px 0 0 20px',
+                borderTop: '0.5px solid #a4a4a4',
+                borderLeft: '0.5px solid #a4a4a4',
+                borderBottom: '0.5px solid #a4a4a4',
+                background: isSelected ? '#633AE2' : '#D9D9D9',
+                boxSizing: 'border-box',
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseEnter={handleMouseEnter}
+        ></div>
+    );
 };
 
 export const TimeSlotRight: React.FC<TimeSlotProps> = ({
-  id,
-  onSelectFromHere,
-  onSelectToHere,
-  isSelected,
+    id,
+    isSelected,
+    onSelectSlot,
+    onMouseEnter,
 }) => {
-  const handleMouseDown = () => {
-    onSelectFromHere(id);
-  };
+    const handleMouseDown = () => {
+        onSelectSlot(id);
+    };
 
-  const handleMouseOver = () => {
-    onSelectToHere(id);
-  };
-  return (
-    <div
-      id={`Time${id}`}
-      style={{
-        display: 'flex',
-        width: '26.665px',
-        height: '39.74px',
-        borderRadius: '0 20px 20px 0',
-        borderLeft: '0.5px dashed #a4a4a4',
-        borderTop: '0.5px solid #a4a4a4',
-        borderRight: '0.5px solid #a4a4a4',
-        borderBottom: '0.5px solid #a4a4a4',
-        background: isSelected ? '#633AE2' : '#D9D9D9',
-        boxSizing: 'border-box',
-      }}
-      onMouseDown={handleMouseDown}
-      onMouseOver={handleMouseOver}
-    ></div>
-  );
+    const handleMouseEnter = () => {
+        onMouseEnter();
+    };
+    return (
+        <div
+            id={`Time${id}`}
+            style={{
+                display: 'flex',
+                width: '26.665px',
+                height: '39.74px',
+                borderRadius: '0 20px 20px 0',
+                borderLeft: '0.5px dashed #a4a4a4',
+                borderTop: '0.5px solid #a4a4a4',
+                borderRight: '0.5px solid #a4a4a4',
+                borderBottom: '0.5px solid #a4a4a4',
+                background: isSelected ? '#633AE2' : '#D9D9D9',
+                boxSizing: 'border-box',
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseEnter={handleMouseEnter}
+        ></div>
+    );
 };
