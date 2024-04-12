e159f9f9fe3724990f6a3edb02eedd037d970f37
ekkk1126 <cabi0203@naver.com>
2024-04-13 03:29:10 +0900

    style : 사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,18 @@ import Login from './Pages/MainPage/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectPage from './Pages/ProjectPage/ProjectPage';
 import StoragePage from './Pages/StoragePage/StoragePage';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const queryClient = new QueryClient();
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="projectmake" element={<ProjectMakePage />} />
-                    <Route path="project" element={<ProjectPage />} />
-                    <Route path="myproject" element={<StoragePage />} />
-                </Routes>
-            </Router>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="projectmake" element={<ProjectMakePage />} />
+                <Route path="project" element={<ProjectPage />} />
+                <Route path="myproject" element={<StoragePage />} />
+            </Routes>
+        </Router>
     );
 }
 
--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,7 +1,6 @@
 import { FC, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Login from './Login';
 import Alarm from './Alarm';
 import NewProject from './NewProject';
 import Search from './Search';
@@ -120,75 +119,6 @@ const MainPage: FC = () => {
                 </div>
             </MainContainer>
 
-            <div
-                style={{
-                    height: '300px',
-                }}
-            />
-        </>
-    );
-    return (
-        <>
-            <GlobalStyle />
-            <BeforeLogin />
-            <Banner />
-            <div
-                style={{
-                    height: '126px',
-                }}
-            />
-            <MainContainer>
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        gap: '20px',
-                    }}
-                >
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'flex-start',
-                            gap: '14px',
-                        }}
-                    >
-                        <Profile />
-                        <Alarm />
-                    </div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'flex-start',
-                            gap: '24px',
-                        }}
-                    >
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'flex-start',
-                                gap: '21px',
-                            }}
-                        >
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    alignItems: 'flex-start',
-                                    gap: '8px',
-                                }}
-                            >
-                                <Search />
-                                <NewProject />
-                            </div>
-                            <Pinned />
-                        </div>
-                        <ProjectList />
-                    </div>
-                </div>
-            </MainContainer>
             <div
                 style={{
                     height: '300px',
