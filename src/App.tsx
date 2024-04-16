f2b6a7955571a285eda14df335a870382e9114c6
ekkk1126 <cabi0203@naver.com>
2024-04-16 11:44:52 +0900

    chore : QueryClientProvider 선언

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import Login from './Pages/MainPage/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectPage from './Pages/ProjectPage/ProjectPage';
 import StoragePage from './Pages/StoragePage/StoragePage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="projectmake" element={<ProjectMakePage />} />
-                <Route path="project" element={<ProjectPage />} />
-                <Route path="myproject" element={<StoragePage />} />
-            </Routes>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="projectmake" element={<ProjectMakePage />} />
+                    <Route path="project" element={<ProjectPage />} />
+                    <Route path="myproject" element={<StoragePage />} />
+                </Routes>
+            </Router>
+        </QueryClientProvider>
     );
 }
 
