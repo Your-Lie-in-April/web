fed6433f12cbd649103ef957e344c74df9f1a5ce
ekkk1126 <cabi0203@naver.com>
2024-05-01 15:22:27 +0900

    chore : 폴더 변경 및 주석 수정

--- src/hooks/apis/queries/schedule/useScheduleMemberQuery.ts
+++ src/hooks/apis/queries/schedule/useScheduleMemberQuery.ts
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '#/constants/queryKey';
 import { getScheduleMember } from '#/apis/schedule';
 /**
- * GET /v1/schedules/all
+ * GET /v1/projects/{projectId}/members/{memberId}/schedules
  *
- * 시간표 전체를 조회하는 api 입니다.
+ * 프로젝트 안에서 특정 유저가 작성한 시간표 출력하는 api 입니다.
  */
 const useScheduleMemberQuery = (projectId: number, memberId: number) => {
     return useQuery({
