6f0fed871a4c32cb23a2d68c42afb2a25d710057
unknown <gojh1013@gmail.com>
2024-05-11 18:10:45 +0900

    feat: 핀설정된 프로젝트 불러오기

--- src/Pages/MainPage/components/Pinned.tsx
+++ src/Pages/MainPage/components/Pinned.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
-
+import { useUserContext } from '../MainPage';
+import { useEffect, useState } from 'react';
+import { PinProjectResponse } from '#/Types/projecttype';
+import { Http } from '#/constants/backendURL';
 const PinnedBox = styled.div`
     width: 950px;
     height: 400px;
@@ -72,7 +75,44 @@ const DetailText = styled.text`
     align-items: flex-start;
 `;
 
-const Pinned = () => {
+const Pinned: React.FC = () => {
+    const { userData } = useUserContext();
+    const [pinnedProjects, setPinnedProjects] = useState<PinProjectResponse | null>(null);
+    useEffect(() => {
+        const accessToken = localStorage.getItem('access_token');
+        const fetchPinnedProjects = async () => {
+            try {
+                const response = await fetch(`${Http}/v1/projects/members/${userData?.memberId}/pin`, {
+                    method: 'GET',
+                    headers: {
+                        Accept: '*/*',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                console.log(userData);
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch pinned projects');
+                }
+
+                const data = await response.json();
+                console.log(data.data[0]);
+                setPinnedProjects(data.data[0]);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchPinnedProjects();
+    }, [userData]);
+
+    const formatDate = (dateStr: number) => {
+        return new Date(dateStr).toLocaleDateString('ko-KR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+    };
+
     return (
         <PinnedBox>
             <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -82,15 +122,14 @@ const Pinned = () => {
                 <ProjectBox>
                     <TextDiv>
                         <ProjectText>
-                            프로젝트명
                             <br />
-                            프로젝트명
+                            {pinnedProjects?.title}
                         </ProjectText>
-                        <DetailText>멤버 00명</DetailText>
+                        <DetailText>멤버 {pinnedProjects?.memberCount}명</DetailText>
                         <DetailText>
                             프로젝트 기간
                             <span />
-                            20XX.XX.XX ~20XX.XX.XX
+                            {pinnedProjects?.startDate} ~{pinnedProjects?.endDate}
                         </DetailText>
                     </TextDiv>
                     <ScheduleBox />
