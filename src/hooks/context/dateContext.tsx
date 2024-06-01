e3d56a3d883093ccced3d74cdf9fbe893b0300c7
ekkk1126 <cabi0203@naver.com>
2024-06-01 19:15:53 +0900

    style : 달력 기본 날짜 -> 오늘날짜

--- src/hooks/context/dateContext.tsx
+++ src/hooks/context/dateContext.tsx
@@ -4,45 +4,33 @@ import { useProjectContext } from './projectContext';
 import { getWeekDates } from '#/Pages/ProjectPage/Schedule/weekUtils';
 
 interface DateContextValue {
-    selectedDate: string | null;
-    setSelectedDate: React.Dispatch<React.SetStateAction<string | null>>;
-    weekDates: string[];
+  selectedDate: string | null;
+  setSelectedDate: React.Dispatch<React.SetStateAction<string | null>>;
+  weekDates: string[];
 }
 
 export const DateContext = createContext<DateContextValue | undefined>(
-    undefined
+  undefined
 );
 
 export const DateProvider: React.FC<{ children: React.ReactNode }> = ({
-    children,
+  children,
 }) => {
-    const today = dayjs(new Date()).format('YYYY-MM-DD');
-    const { projectData } = useProjectContext();
-    const [selectedDate, setSelectedDate] = useState<string | null>(today);
-    const [weekDates, setWeekDates] = useState<string[]>([]);
+  const today = dayjs(new Date()).format('YYYY-MM-DD');
+  const { projectData } = useProjectContext();
+  const [selectedDate, setSelectedDate] = useState<string | null>(today);
+  const [weekDates, setWeekDates] = useState<string[]>([]);
 
-    useEffect(() => {
-        if (projectData) {
-            const startDateString = projectData.startDate;
-            const startDate = startDateString
-                ? dayjs(startDateString).format('YYYY-MM-DD')
-                : today;
-            setSelectedDate(startDate);
-        }
-    }, [projectData, today]);
+  useEffect(() => {
+    if (selectedDate) {
+      const week = getWeekDates(selectedDate);
+      setWeekDates(week);
+    }
+  }, [selectedDate]);
 
-    useEffect(() => {
-        if (selectedDate) {
-            const week = getWeekDates(selectedDate);
-            setWeekDates(week);
-        }
-    }, [selectedDate]);
-
-    return (
-        <DateContext.Provider
-            value={{ selectedDate, setSelectedDate, weekDates }}
-        >
-            {children}
-        </DateContext.Provider>
-    );
+  return (
+    <DateContext.Provider value={{ selectedDate, setSelectedDate, weekDates }}>
+      {children}
+    </DateContext.Provider>
+  );
 };
