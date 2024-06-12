a1ebea00e01b2bfefc754581117e56da8349dc66
unknown <gojh1013@gmail.com>
2024-06-12 18:03:38 +0900

    feat: 로그아웃 시 로그인페이지로 리다이렉션

--- src/Pages/Layouts/AfterLogin.tsx
+++ src/Pages/Layouts/AfterLogin.tsx
@@ -5,85 +5,82 @@ import ConfirmLogout from '../Modal/ConfirmLogout';
 import MyPageModal from '../Modal/MyPageModal';
 
 const AfterLoginDiv = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: #f1f1f1;
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  z-index: 2;
-  color: #000000;
-  font-style: normal;
+    width: 100%;
+    height: 100px;
+    background-color: #f1f1f1;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    z-index: 2;
+    color: #000000;
+    font-style: normal;
 `;
 
 const Logo = styled.div`
-  position: relative;
-  bottom: 14px;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  font-weight: 700;
-  font-size: 32px;
+    position: relative;
+    bottom: 14px;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    font-weight: 700;
+    font-size: 32px;
 `;
 
 const MyPageDiv = styled.div`
-  position: relative;
-  bottom: 21px;
-  justify-content: flex-end;
-  flex-basis: 20%;
-  white-space: nowrap;
+    position: relative;
+    bottom: 21px;
+    justify-content: flex-end;
+    flex-basis: 20%;
+    white-space: nowrap;
 `;
 
 const MyPageBtn = styled.button`
-  box-sizing: border-box;
-  color: #000000;
-  font-weight: 500;
-  font-size: 22px;
+    box-sizing: border-box;
+    color: #000000;
+    font-weight: 500;
+    font-size: 22px;
 
-  background-color: transparent;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-
-  &:focus {
+    background-color: transparent;
     border: none;
-    outline: none;
-  }
+    padding: 0;
+    cursor: pointer;
+
+    &:focus {
+        border: none;
+        outline: none;
+    }
 `;
 
 const AfterLogin: FC = () => {
-  const [isLogout, setLogOut] = useState<boolean>(false);
-  const [isMyPageModal, setIsMyPageModal] = useState(false);
+    const [isLogout, setLogOut] = useState<boolean>(false);
+    const [isMyPageModal, setIsMyPageModal] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const onSetIsMyPageModal = () => {
-    setIsMyPageModal((prev) => !prev);
-  };
+    const onSetIsMyPageModal = () => {
+        setIsMyPageModal((prev) => !prev);
+    };
 
-  const onSetIsLogout = () => {
-    setLogOut((prev) => !prev);
-  };
+    const onSetIsLogout = () => {
+        setLogOut(true);
+    };
 
-  return (
-    <>
-      <AfterLoginDiv>
-        <div style={{ justifyContent: 'flex-start', flexBasis: '20%' }} />
-        <Logo onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>
-          TIME PIECE
-        </Logo>
-        <MyPageDiv>
-          <MyPageBtn onClick={onSetIsMyPageModal}>My page</MyPageBtn>
-          {isMyPageModal && (
-            <MyPageModal
-              onSetIsMyPageModal={onSetIsMyPageModal}
-              onSetIsLogout={onSetIsLogout}
-            />
-          )}
-        </MyPageDiv>
-      </AfterLoginDiv>
-      {isLogout && <ConfirmLogout setLogOut={setLogOut} />}
-    </>
-  );
+    return (
+        <>
+            <AfterLoginDiv>
+                <div style={{ justifyContent: 'flex-start', flexBasis: '20%' }} />
+                <Logo onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>
+                    TIME PIECE
+                </Logo>
+                <MyPageDiv>
+                    <MyPageBtn onClick={onSetIsMyPageModal}>My page</MyPageBtn>
+                    {isMyPageModal && (
+                        <MyPageModal onSetIsMyPageModal={onSetIsMyPageModal} onSetIsLogout={onSetIsLogout} />
+                    )}
+                </MyPageDiv>
+            </AfterLoginDiv>
+            {isLogout && <ConfirmLogout setLogOut={setLogOut} />}
+        </>
+    );
 };
 export default AfterLogin;
