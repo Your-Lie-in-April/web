9a86987f03d1f94fffddd3867d45c5086f9d9f73
ekkk1126 <cabi0203@naver.com>
2024-06-20 02:06:10 +0900

    style : import  정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import MainPage from './Pages/MainPage/MainPage';
-import ProjectMakePage from './Pages/ProjectMakePage/projectmakepage';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Login/Login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage, { UserProvider } from './Pages/MainPage/MainPage';
+import ProjectMakePage from './Pages/ProjectMakePage/projectmakepage';
 import ProjectPage from './Pages/ProjectPage/ProjectPage';
 import StoragePage from './Pages/StoragePage/StoragePage';
-import { UserProvider } from './Pages/MainPage/MainPage';
 import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
-    const URL = '/';
     return (
         <UserProvider>
             <Router>
                 <ScrollToTop />
                 <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="projectmake" element={<ProjectMakePage />} />
+                    <Route path='/' element={<MainPage />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='projectmake' element={<ProjectMakePage />} />
                     <Route
-                        path="project/:projectId"
+                        path='project/:projectId'
                         element={<ProjectPage />}
                     />
-                    <Route path="myproject" element={<StoragePage />} />
+                    <Route path='myproject' element={<StoragePage />} />
                 </Routes>
             </Router>
         </UserProvider>
