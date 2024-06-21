a982b10ff79ee55f77fb974a4be65c6874c26b7c
unknown <gojh1013@gmail.com>
2024-06-21 16:39:39 +0900

    feat: 프로젝트 삭제시 재로딩

--- src/Pages/Modal/DeleteProject.tsx
+++ src/Pages/Modal/DeleteProject.tsx
@@ -6,167 +6,162 @@ import styled from 'styled-components';
 import { ModalBlackOut, ModalContainer } from './ModalCommon';
 
 const Box = styled.div`
-  width: 406px;
-  min-height: 182px;
-  border-radius: 20px;
-  background: #f5f5f5;
-  box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  padding: 16px 20px 8px 20px;
-  box-sizing: border-box;
+    width: 406px;
+    min-height: 182px;
+    border-radius: 20px;
+    background: #f5f5f5;
+    box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 20px 8px 20px;
+    box-sizing: border-box;
 `;
 
 const InfoCircleIcon = styled(InfoOutlinedIcon)`
-  width: 32px;
-  height: 32px;
-  color: #eb5757;
+    width: 32px;
+    height: 32px;
+    color: #eb5757;
 `;
 
 const CommonText = styled.text`
-  color: #000000;
-  text-align: center;
-  font-family: Pretendard;
-  line-height: normal;
+    color: #000000;
+    text-align: center;
+    font-family: Pretendard;
+    line-height: normal;
 `;
 
 const PeojectName = styled(CommonText)`
-  font-size: 28px;
-  font-weight: 700;
+    font-size: 28px;
+    font-weight: 700;
 
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 1;
-  white-space: pre-wrap;
-  word-break: break-word;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    white-space: pre-wrap;
+    word-break: break-word;
 `;
 
 const Title = styled(CommonText)`
-  font-size: 24px;
-  font-weight: 400;
+    font-size: 24px;
+    font-weight: 400;
 `;
 
 const Button = styled.button`
-  display: flex;
-  width: 60px;
-  padding: 8px 12px;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  border-radius: 20px;
-  font-family: Pretendard;
-  font-size: 13px;
-  font-weight: 500;
-  line-height: normal;
+    display: flex;
+    width: 60px;
+    padding: 8px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    border-radius: 20px;
+    font-family: Pretendard;
+    font-size: 13px;
+    font-weight: 500;
+    line-height: normal;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
 `;
 
 const ConfirmBtn = styled(Button)`
-  background: #633ae2;
-  color: #ffffff;
+    background: #633ae2;
+    color: #ffffff;
 `;
 
 const CancelBtn = styled(Button)`
-  background: #d9d9d9;
-  color: #ffffff;
+    background: #d9d9d9;
+    color: #ffffff;
 `;
 
 const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 4px;
+    display: flex;
+    justify-content: flex-end;
+    gap: 4px;
 `;
 
 interface DeleteProjectProps {
-  projectId: number;
-  title: string;
-  isClick: boolean;
-  onClose: () => void;
+    projectId: number;
+    title: string;
+    isClick: boolean;
+    onClose: () => void;
 }
 
-const DeleteProject: React.FC<DeleteProjectProps> = ({
-  projectId,
-  title,
-  isClick,
-  onClose,
-}) => {
-  const deleteProject = async () => {
-    try {
-      const accessToken = localStorage.getItem('access_token');
-      if (accessToken) {
-        const response = await fetch(`${Http}/v1/projects/${projectId}`, {
-          method: 'DELETE',
-          headers: {
-            Accept: '*/*',
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        if (!response.ok) {
-          console.log('failed to delete storage project');
+const DeleteProject: React.FC<DeleteProjectProps> = ({ projectId, title, isClick, onClose }) => {
+    const deleteProject = async () => {
+        try {
+            const accessToken = localStorage.getItem('access_token');
+            if (accessToken) {
+                const response = await fetch(`${Http}/v1/projects/${projectId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        Accept: '*/*',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                if (!response.ok) {
+                    console.log('failed to delete storage project');
+                }
+                const data = await response.json();
+                window.location.reload();
+            }
+        } catch (error) {
+            console.log(error);
         }
-        const data = await response.json();
-        console.log('삭제삭제삭제', data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
 
-  const onConfirmDelete = () => {
-    deleteProject();
-    onClose();
-  };
+    const onConfirmDelete = () => {
+        deleteProject();
+        onClose();
+    };
 
-  useScrollLock(isClick);
+    useScrollLock(isClick);
 
-  return (
-    <>
-      {isClick && (
-        <ModalPortal>
-          <ModalBlackOut onClick={onClose} />
-          <ModalContainer>
-            <Box>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  gap: '12px',
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    gap: '10px',
-                    width: '100%',
-                    height: '100%',
-                  }}
-                >
-                  <InfoCircleIcon sx={{ fontSize: '32px' }} />
-                  <PeojectName>{title}</PeojectName>
-                  <Title>해당 프로젝트를 삭제하겠습니까?</Title>
-                </div>
-                <ButtonsContainer style={{ alignSelf: 'flex-end' }}>
-                  <ConfirmBtn onClick={onConfirmDelete}>확인</ConfirmBtn>
-                  <CancelBtn onClick={onClose}>취소</CancelBtn>
-                </ButtonsContainer>
-              </div>
-            </Box>
-          </ModalContainer>
-        </ModalPortal>
-      )}
-    </>
-  );
+    return (
+        <>
+            {isClick && (
+                <ModalPortal>
+                    <ModalBlackOut onClick={onClose} />
+                    <ModalContainer>
+                        <Box>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    gap: '12px',
+                                    width: '100%',
+                                    height: '100%',
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        gap: '10px',
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                >
+                                    <InfoCircleIcon sx={{ fontSize: '32px' }} />
+                                    <PeojectName>{title}</PeojectName>
+                                    <Title>해당 프로젝트를 삭제하겠습니까?</Title>
+                                </div>
+                                <ButtonsContainer style={{ alignSelf: 'flex-end' }}>
+                                    <ConfirmBtn onClick={onConfirmDelete}>확인</ConfirmBtn>
+                                    <CancelBtn onClick={onClose}>취소</CancelBtn>
+                                </ButtonsContainer>
+                            </div>
+                        </Box>
+                    </ModalContainer>
+                </ModalPortal>
+            )}
+        </>
+    );
 };
 
 export default DeleteProject;
--- src/Pages/StoragePage/StoragePage.tsx
+++ src/Pages/StoragePage/StoragePage.tsx
@@ -24,130 +24,119 @@ body {
 `;
 
 const Title = styled.div`
-  color: #ffffff;
-  text-align: center;
-  font-size: 42px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
+    color: #ffffff;
+    text-align: center;
+    font-size: 42px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
 `;
 
 const StoragePage = () => {
-  const [storelist, setStoreList] = useState<ProjectThumbnailResponse[]>([]);
-  const [searchResults, setSearchResults] = useState<
-    ProjectThumbnailResponse[]
-  >([]);
-  const [page, setPage] = useState<number>(0);
-  const [size] = useState<number>(9);
-  const [hasMore, setHasMore] = useState<boolean>(true);
+    const [storelist, setStoreList] = useState<ProjectThumbnailResponse[]>([]);
+    const [searchResults, setSearchResults] = useState<ProjectThumbnailResponse[]>([]);
+    const [page, setPage] = useState<number>(0);
+    const [size] = useState<number>(9);
+    const [hasMore, setHasMore] = useState<boolean>(true);
 
-  const observer = useRef<IntersectionObserver | null>(null);
-  const navigate = useNavigate();
+    const observer = useRef<IntersectionObserver | null>(null);
 
-  const lastProjectRef = useCallback(
-    (node: HTMLDivElement | null) => {
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPage((prevPage) => prevPage + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [hasMore]
-  );
-
-  const fetchStoredProjects = async () => {
-    try {
-      const accessToken = localStorage.getItem('access_token');
-      if (accessToken) {
-        const response = await fetch(
-          `${Http}/v1/projects/stored?page=${page}&size=${size}`,
-          {
-            method: 'GET',
-            headers: {
-              Accept: '*/*',
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-        if (!response.ok) {
-          console.log('failed to fetch storage projects');
-        }
-        const data = await response.json();
-        console.log(data);
-        if (page === 0) {
-          setStoreList(data.data);
-          setSearchResults(data.data);
-        } else {
-          setStoreList((prevProjects) => [...prevProjects, ...data.data]);
-          setSearchResults((prevProjects) => [...prevProjects, ...data.data]);
-        }
-        setHasMore(data.data.length > 0);
-      } 
-      // else {
-      //   navigate('/login');
-      //   return;
-      // }
-    } catch (error) {
-      console.error('업데이트 실패:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchStoredProjects();
-  }, [page, size]);
-
-  const handleSearch = (query: string) => {
-    const searchProjects = storelist.filter((project) =>
-      project.title.toLowerCase().includes(query.toLowerCase())
+    const lastProjectRef = useCallback(
+        (node: HTMLDivElement | null) => {
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasMore) {
+                    setPage((prevPage) => prevPage + 1);
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [hasMore]
     );
-    setSearchResults(searchProjects);
-  };
 
-  return (
-    <>
-      <GlobalStyle />
-      <>
-        <GraphicIcons />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '130px',
-          }}
-        >
-          <AfterLogin />
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '49px',
-              alignItems: 'center',
+    const fetchStoredProjects = async () => {
+        try {
+            const accessToken = localStorage.getItem('access_token');
+            if (accessToken) {
+                const response = await fetch(`${Http}/v1/projects/stored?page=${page}&size=${size}`, {
+                    method: 'GET',
+                    headers: {
+                        Accept: '*/*',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                if (!response.ok) {
+                    console.log('failed to fetch storage projects');
+                }
+                const data = await response.json();
+                console.log(data);
+                if (page === 0) {
+                    setStoreList(data.data);
+                    setSearchResults(data.data);
+                } else {
+                    setStoreList((prevProjects) => [...prevProjects, ...data.data]);
+                    setSearchResults((prevProjects) => [...prevProjects, ...data.data]);
+                }
+                setHasMore(data.data.length > 0);
+            }
+            // else {
+            //   navigate('/login');
+            //   return;
+            // }
+        } catch (error) {
+            console.error('업데이트 실패:', error);
+        }
+    };
 
-              zIndex: '1',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '48px',
-              }}
-            >
-              <Title>프로젝트 보관함</Title>
-              <Search onSearch={handleSearch} />
-            </div>
-            <StorageProjectList
-              projects={searchResults}
-              lastProjectRef={lastProjectRef}
-            />
-          </div>
-        </div>
-        <div style={{ width: '100vw', height: '172px' }}></div>
-      </>
-    </>
-  );
+    useEffect(() => {
+        fetchStoredProjects();
+    }, [page, size]);
+
+    const handleSearch = (query: string) => {
+        const searchProjects = storelist.filter((project) => project.title.toLowerCase().includes(query.toLowerCase()));
+        setSearchResults(searchProjects);
+    };
+
+    return (
+        <>
+            <GlobalStyle />
+            <>
+                <GraphicIcons />
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '130px',
+                    }}
+                >
+                    <AfterLogin />
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '49px',
+                            alignItems: 'center',
+
+                            zIndex: '1',
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: '48px',
+                            }}
+                        >
+                            <Title>프로젝트 보관함</Title>
+                            <Search onSearch={handleSearch} />
+                        </div>
+                        <StorageProjectList projects={searchResults} lastProjectRef={lastProjectRef} />
+                    </div>
+                </div>
+                <div style={{ width: '100vw', height: '172px' }}></div>
+            </>
+        </>
+    );
 };
 
 export default StoragePage;
