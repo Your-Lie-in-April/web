4abaebac8f48ebb0fd06f23cd08a8715a0ea98a9
ekkk1126 <cabi0203@naver.com>
2024-07-18 20:18:18 +0900

    delete : .git cashed .

--- src/route/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import getIsLogin from '@utils/getIsLogin';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-    return getIsLogin() ? <>{children}</> : <Navigate to='/login' replace />;
-};
-
-export default PrivateRoute;
--- src/utils/getIsLogin.ts
+++
@@ -1,3 +0,0 @@
-export default function getIsLogin() {
-    return !!localStorage.getItem('access_token');
-}
