a85b49215462b138f372f8f5cdf7216b794463aa
ekkk1126 <cabi0203@naver.com>
2024-08-11 22:58:58 +0900

    remove : 기본 프로필 이미지 중복 파일 삭제

--- src/pages/project/components/profiles/MyProfile.tsx
+++ src/pages/project/components/profiles/MyProfile.tsx
@@ -1,7 +1,7 @@
 import useAllMemberInfoQuery from '@hooks/apis/queries/member/useAllMemberInfoQuery';
 import { useUserContext } from '@hooks/context/userContext';
 import ChangeNickName from '@pages/modal/member/ChangeNickname';
-import defaultProfile from '@pics/default.png';
+import defaultProfile from '@pics/default-profile.svg';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
