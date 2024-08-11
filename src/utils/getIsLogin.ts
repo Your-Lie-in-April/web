ab985cf9b6f22725d23b50ca0770005cf5594397
ekkk1126 <cabi0203@naver.com>
2024-08-11 23:53:01 +0900

    add : 로그인 조건 추가

--- src/utils/getIsLogin.ts
+++ src/utils/getIsLogin.ts
@@ -1,3 +1,7 @@
 export default function getIsLogin() {
-    return !!localStorage.getItem('access_token');
+    return (
+        !!localStorage.getItem('access_token') &&
+        !!localStorage.getItem('refresh_token') &&
+        !!localStorage.getItem('member_id')
+    );
 }
